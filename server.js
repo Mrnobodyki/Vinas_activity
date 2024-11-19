ed61123db6bac2c0c1f1360ca3adfd1309fa3106
pomasinrichie <pomasinrichie@gmail.com>
2024-11-19 16:08:33 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+let mockData  = [
+    { id: 1, name: "Product A", price: 10 },
+    { id: 2, name: "Product B", price: 20 },
+    { id: 3, name: "Product C", price: 30 },
+  ];
+  
+  app.get("/items", (req, res) => {
+    res.json(mockData); // to send the books array as a response
+  });
+
+  
